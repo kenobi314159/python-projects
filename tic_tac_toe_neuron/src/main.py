bbeb044c09e57d0f671b60caba2051098d6ee341
Jan Kubalek <kubalekj492@gmail.com>
2026-06-26 09:42:29 +0200

    tic_tac_toe: src/main: Update parameters

--- tic_tac_toe_neuron/src/main.py
+++ tic_tac_toe_neuron/src/main.py
@@ -24,7 +24,12 @@ from training import *
 # Customized training of a model with some specific parameters
 def trainCustomModel0(
     name="model0",
-    conv_layers=5,
+    input_width = 4,
+    conv3x3_layers=10,
+    conv3x3_channels=4,
+    conv5x5_layers=10,
+    conv5x5_channels=4,
+    input_to_dense_propagate=True,
     dense_layers=3,
 
     max_training_data_size=50,
@@ -53,9 +58,8 @@ def trainCustomModel0(
     model_name = f"{name}_{model_grid_size}x{model_grid_size}"
 
     if (start_new):
-        conv_layers = conv_layers
         dense_layers = dense_layers
-        width = 4
+        width = input_width
         func0 = "elu"
         func1 = "tanh"
         func2 = "relu"
@@ -68,7 +72,11 @@ def trainCustomModel0(
         m_hero = createCnnModel(
             model_grid_size,
             model_grid_size,
-            conv_layers=conv_layers,
+            conv3x3_layers=conv3x3_layers,
+            conv3x3_channels=conv3x3_channels,
+            conv5x5_layers=conv5x5_layers,
+            conv5x5_channels=conv5x5_channels,
+            input_to_dense_propagate=input_to_dense_propagate,
             dense_sizes=dense_sizes,
             conv_activation=func3,
             mid_activations=mid_activations,
@@ -81,7 +89,7 @@ def trainCustomModel0(
         storage = ModelStorage()
         m_hero, fn_hero = storage.loadLatesModelContaining(model_name)
         print(f"Loading model {fn_hero}")
-        numbering = fn_hero.split(model_name)[1].split(".")[0]
+        numbering = fn_hero.split(model_name)[1].split(".")[1]
         model_games = int(numbering.split("_")[1])
         model_inputs_trained = int(numbering.split("_")[2])
 
@@ -125,10 +133,6 @@ if __name__ == "__main__":
                         help="Instead of training, test different versions of the model against each other. Requires 4 parameter values: model name prefix, number of versions, rounds per test, number of parallel threads to use for testing. Overrides all training-related arguments.")
     parser.add_argument("--model-name",
         default="model0", help="Model name prefix to use for training or testing. Default value is 'model0'.")
-    parser.add_argument("--conv-layers",
-        type=int, default=5, help="Number of convolutional layers in the model. Default value is 5.")
-    parser.add_argument("--dense-layers",
-        type=int, default=3, help="Number of dense layers in the model. Default value is 3.")
     parser.add_argument("--max-training-data-size",
         type=int, default=50, help="Maximum training data size multiplier. Default value is 50.")
     parser.add_argument("--train-interval",
@@ -159,8 +163,23 @@ if __name__ == "__main__":
         type=float, default=0.2, help="Mistake rate for Fred player during testing. Default value is 0.2.")
     parser.add_argument("--test-runs",
         type=int, default=50, help="Number of test runs to perform during testing against Fred. Default value is 50.")
+
     parser.add_argument("--start-new",
         action="store_true", help="Start training a new model instead of loading the latest one.")
+    parser.add_argument("--input-width",
+        type=int, default=4, help="Width of the input data for the model. Default value is 4.")
+    parser.add_argument("--conv3x3-layers",
+        type=int, default=1, help="Number of 3x3 convolutional layers in the model. Default value is 1.")
+    parser.add_argument("--conv3x3-channels",
+        type=int, default=5, help="Number of channels in 3x3 convolutional layers. Default value is 5.")
+    parser.add_argument("--conv5x5-layers",
+        type=int, default=1, help="Number of 5x5 convolutional layers in the model. Default value is 1.")
+    parser.add_argument("--conv5x5-channels",
+        type=int, default=5, help="Number of channels in 5x5 convolutional layers. Default value is 5.")
+    parser.add_argument("--input-to-dense-propagate",
+        action="store_true", default=True, help="Propagate input directly to dense layers in the model. Default is True.")
+    parser.add_argument("--dense-layers",
+        type=int, default=4, help="Number of dense layers in the model. Default value is 4.")
 
     args = parser.parse_args()
 
@@ -178,31 +197,46 @@ if __name__ == "__main__":
         testModelVersions(name, versions_num, rounds_per_test, parallel_threads)
         exit(0)
 
+    #model_name = f"model-t12-0_3x3-1x10_5x5-1x10"
+    #storage = ModelStorage()
+    #model_files = [fn for fn in storage.getStorageList() if model_name in fn]
+    #for m in model_files:
+    #    m_hero, fn_hero = storage.loadLatesModelContaining(m)
+    #    print(f"Loading model {fn_hero}")
+    #    print(m_hero.summary())
+    #exit()
+
     trainCustomModel0(
-    name="model-t9-5",
-    conv_layers=10,
-    dense_layers=4,
+    name=f"model-t12-0_3x3-{args.conv3x3_layers}x{args.conv3x3_channels}_5x5-{args.conv5x5_layers}x{args.conv5x5_channels}_{str(args.input_to_dense_propagate)}",
+
+    start_new=args.start_new,
+    input_width=args.input_width,
+    conv3x3_layers=args.conv3x3_layers,
+    conv3x3_channels=args.conv3x3_channels,
+    conv5x5_layers=args.conv5x5_layers,
+    conv5x5_channels=args.conv5x5_channels,
+    input_to_dense_propagate=args.input_to_dense_propagate,
+    dense_layers=args.dense_layers,
+
+    load_only=False,
 
     max_training_data_size=1000,
-    train_interval=20*60,
-    serial_rounds=100,
+    train_interval=2*60,
+    serial_rounds=40,
     threads_num=8,
     shortest_cutoff=0,
     winner_weight=1.0,
     loser_weight=0.0,
-    learning_rate=0.000002,
-    player_training_variants=200,
-    top_random_select_size=10,
+    learning_rate=0.00001,
+    player_training_variants=100,
+    top_random_select_size=3,
     top_select_equal=True,
-    weights_scale_coef=0.0,
-    weights_scale_uniform=False,
+    weights_scale_coef=1.0,
+    weights_scale_uniform=True,
     train_against_best=True,
 
     fred_mistake_rate=0.2,
     test_runs=50,
-
-    start_new=False,
-    load_only=False,
     )
 
     #testModelVersions("hero_4", 3, 4, 8)
